7578951b72979581bcf4eec324827a62c6c0785c
Robinjud <rbnjd7@gmail.com>
2023-11-08 16:58:47 -0600

    base template extraction

--- store/views.py
+++ store/views.py
@@ -3,11 +3,15 @@ from .models import Book
 
 # Create your views here.
 def index(request):
-    return render(request, 'store/template.html')
+    context = {
+        'page': 'Mystery Books'
+    }
+    return render(request, 'store/template.html', context)
 
 def store(request):
     count = Book.objects.all().count()
     context = {
         'count': count,
+        'page': 'Welcome to Mistery Books'
     } 
     return render(request, "store/store.html", context)
