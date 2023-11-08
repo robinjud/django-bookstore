76e87ad0cb38c18bd9e9410d386ec85d4f99bb0a
Robinjud <rbnjd7@gmail.com>
2023-11-08 16:22:32 -0600

    data model and querying the database

--- store/views.py
+++ store/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def index(request):
     return render(request, 'store/template.html')
 
 def store(request):
-    return render(request, 'store/store.html')
+    count = Book.objects.all().count()
+    context = {
+        'count': count,
+    } 
+    return render(request, "store/store.html", context)
