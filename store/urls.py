f89d7954b5f07dde87deefce13921f07c91e924d
Robinjud <rbnjd7@gmail.com>
2023-11-07 19:44:06 -0600

    apps and templates

--- store/urls.py
+++ store/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('store.urls')),
-    path('admin/', admin.site.urls),
+    path('',views.index, name='index'),
+    path('store/', views.store, name='store'),
 ]
